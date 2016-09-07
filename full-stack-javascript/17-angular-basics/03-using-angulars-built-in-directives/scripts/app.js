93f037f4d18898ca0f721c483153a67164e2d247
Sean M Hamlet <seanmhamlet@gmail.com>
2016-09-07 07:02:00 -0400

    Fixes intial commit by removing old code

--- full-stack-javascript/17-angular-basics/03-using-angulars-built-in-directives/scripts/app.js
+++ full-stack-javascript/17-angular-basics/03-using-angulars-built-in-directives/scripts/app.js
@@ -4,20 +4,4 @@ angular.module("todoListApp", [])
 	$scope.helloWorld = function() {
 		console.log("Hello there! This is the helloWorld controller function, in the mainCtrl!");
 	};
-})
-
-.controller('coolCtrl', function($scope) {
-	$scope.whoAmI = function() {
-		console.log("Hello there, this is coolCtrl function!");
-	};
-
-	$scope.helloWorld = function() {
-		console.log("This is not the mainCtrl!");
-	};
-})
-
-.controller('imASibling', function($scope) {
-	$scope.foobar = '1234';
-
-	// do other cool stuff
 });
