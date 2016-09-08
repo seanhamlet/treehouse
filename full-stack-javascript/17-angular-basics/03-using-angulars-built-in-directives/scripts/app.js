3e7945abf917cb5061bae68e404556153f38153a
Sean M Hamlet <seanmhamlet@gmail.com>
2016-09-08 08:14:42 -0400

    Adds todos HTML using ng-repeat directive

--- full-stack-javascript/17-angular-basics/03-using-angulars-built-in-directives/scripts/app.js
+++ full-stack-javascript/17-angular-basics/03-using-angulars-built-in-directives/scripts/app.js
@@ -4,4 +4,14 @@ angular.module("todoListApp", [])
 	$scope.helloWorld = function() {
 		console.log("Hello there! This is the helloWorld controller function, in the mainCtrl!");
 	};
+
+	$scope.todos = [
+		{"name": "clean the house"},
+		{"name": "water the dog"},
+		{"name": "feed the lawn"},
+		{"name": "pay dem bills"},
+		{"name": "run"},
+		{"name": "swim"}
+	];
+	
 });
