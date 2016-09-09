2592c2404f2a66157228173222abac0a3cb2e2da
Sean M Hamlet <seanmhamlet@gmail.com>
2016-09-09 13:31:54 -0400

    Scaffolds app into file/folder structure

---
+++ full-stack-javascript/17-angular-basics/05-improving-our-todo-app/scripts/controllers/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// don't use second parameter because we are using the 
+// todolistapp; using a second parameter would create a module called todolistapp
+// thus, this code attaches a new controller to an existing module.
+angular.module('todoListApp') 
+.controller('mainCtrl', function($scope, dataService) {
+
+	$scope.helloConsole = dataService.helloConsole;
+
+	$scope.addTodo = function() {
+    var todo = {"name": "This is a new todo."};
+    $scope.todos.push(todo);
+  };
+
+	dataService.getTodos(function(response) {
+    console.log(response.data);
+    $scope.todos = response.data;
+  });
+
+  $scope.deleteTodo = function(todo, $index) {
+    dataService.deleteTodo(todo);
+    $scope.todos.splice($index, 1);
+  };
+
+  $scope.saveTodo = function(todo) {
+    dataService.saveTodo(todo);
+  };
+
+});
