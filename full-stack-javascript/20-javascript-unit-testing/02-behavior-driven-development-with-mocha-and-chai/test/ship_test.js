b17508cf992d83084725404f4ac8067fe9a7b07b
Sean M Hamlet <seanmhamlet@gmail.com>
2016-09-16 14:04:12 -0400

    Adds basic checkForShip function

---
+++ full-stack-javascript/20-javascript-unit-testing/02-behavior-driven-development-with-mocha-and-chai/test/ship_test.js
@@ -0,0 +1,17 @@
+var expect = require('chai').expect;
+
+describe('checkForShip', function() {
+  var checkForShip = require('../game_logic/ship_methods').checkForShip;
+
+  it("should correctly report no ship at a given player's coordinate", function() {
+
+    player = {
+      ships: [
+        {
+          locations: [[0, 0]]
+        }
+      ]
+    }
+    expect(checkForShip(player, [9, 9])).to.be.false;
+  });
+});
