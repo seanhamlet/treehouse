dbb69d59dcab48bfc9c6ac790fdb586e79c02090
Sean M Hamlet <seanmhamlet@gmail.com>
2016-09-18 14:08:17 -0400

    Adds pending game_test tests

---
+++ full-stack-javascript/20-javascript-unit-testing/04-next-steps/test/game_test.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+
+describe('GAME INSTANCE FUNCTIONS', function() {
+  describe('checkGameStatus', function() {
+    it('should tell me when the game is over');
+  });
+  it('some other stuff');
+  //removing the call back function makes the test pending
+  //placing an 'x' in front of 'describe' or 'it' will make that test pending even if it does have a callback function
+});
